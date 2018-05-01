ade7f562cdbd8c2a483da231a14dbbfe57070a9b
hishshah <ghishshah@yahoo.com>
2018-05-01 23:33:13 +0700

    filter (radio button)

---
+++ drawChart.php
@@ -0,0 +1,58 @@
+<!-- READ CSV FILE -->
+<?php include 'readcsv.php'; ?>
+
+<script type="text/javascript">
+	function getData() {
+		var val = $("input:radio[name=medal]:checked").val()
+		// console.log(val);
+
+		// PHP array to JS array
+		<?php
+			$php_array = $country_array['Thailand'];
+			$js_array = json_encode($php_array);
+			echo "var country_array = ". $js_array . ";\n";
+		?>
+
+		// filtering
+		for (var i = 0; i < country_array.length; i++) {
+			var cabor_array = country_array[i];
+			for (var j = 1; j < cabor_array.length; j++) { // string to int
+				cabor_array[j] = parseInt(cabor_array[j]);
+			}
+
+			if (val == "gold") {
+				cabor_array.splice(2, 2);
+			} else if (val == "silver") {
+				cabor_array.splice(1, 1);
+				cabor_array.splice(2, 1);
+			} else if (val == "brown") {
+				cabor_array.splice(1, 2);
+			}
+			
+			cabor_array.push(val);
+		}
+		// console.log(country_array);
+
+		// chart
+		google.charts.load("current", {packages:["corechart"]});
+		google.charts.setOnLoadCallback(drawChart);
+
+		var header = [["Country", "Medal", { role: "style" }]];
+		var d = header.concat(country_array);
+		// console.log(d);
+
+		function drawChart() {
+		  var data = google.visualization.arrayToDataTable(d);
+
+		  var view = new google.visualization.DataView(data);
+		  
+		  var options = {
+		    legend: { position: "none" },
+		    hAxis: { textPosition: "none"},
+		    // vAxis: { textPosition: "none"},
+		  };
+		  var chart = new google.visualization.BarChart(document.getElementById("barchart_values"));
+		  chart.draw(view, options);
+		}
+	}
+</script>
--- index.php
+++ index.php
@@ -23,64 +23,16 @@
 	<body>
 		<h1 class="title">SEA GAMES OVERVIEW</h1>
 		<div class="controller">
-			<div class="form-check">
-				<input type="checkbox" class="form-check-input" id="cabor1">
-				<label class="form-check-label" for="exampleCheck1">Cabor 1</label>
-			</div>
-			<div class="form-check">
-				<input type="checkbox" class="form-check-input" id="cabor1">
-				<label class="form-check-label" for="exampleCheck2">Cabor 2</label>
-			</div>
+			<form action="JavaScript:getData()">
+				<input type="radio" id="medal" name="medal" value="gold" /> Gold<br/>
+				<input type="radio" id="medal" name="medal" value="silver" /> Silver<br/>
+				<input type="radio" id="medal" name="medal" value="brown" /> Bronze<br/>
+				<input type="submit" value="Apply" class="btn btn-info">
+			</form>
 		</div>
-		<div class="container">
-		    <div class="table-responsive">
-				<table style="width: 100%">
-					<!-- READ CSV FILE -->
-					<?php include 'readcsv.php'; ?>
-
-					<script type="text/javascript">
-						// PHP array to JS array
-						<?php
-							$php_array = $country_array['Thailand'];
-							$js_array = json_encode($php_array);
-							echo "var country_array = ". $js_array . ";\n";
-						?>
-
-						// string to int (for gold, silver, and bronze col)
-						for (var i = 0; i < country_array.length; i++) {
-							var cabor_array = country_array[i];
-							for (var j = 1; j < cabor_array.length; j++) {
-								cabor_array[j] = parseInt(cabor_array[j]);
-							}
-						}
-						// console.log(country_array);
-
-						// chart
-						google.charts.load("current", {packages:["corechart"]});
-						google.charts.setOnLoadCallback(drawChart);
-
-						var header = [["Country", "Gold", "Silver", "Bronze"]];
-						var d = header.concat(country_array);
-						// console.log(d);
-
-						function drawChart() {
-						  var data = google.visualization.arrayToDataTable(d);
-
-						  var view = new google.visualization.DataView(data);
-						  
-						  var options = {
-						    legend: { position: "none" },
-						    hAxis: { textPosition: "none" },
-						    vAxis: { textPosition: "none" },
-						  };
-						  var chart = new google.visualization.BarChart(document.getElementById("barchart_values"));
-						  chart.draw(view, options);
-						}
-					</script>
-
-					<div id="barchart_values" style="width: 900px; height: 500px;"></div>
-				</table>
-			</div>
-		  </div>
+  	<table style="width: 100%">
+			<?php include 'drawChart.php'; ?>
+			<div id="barchart_values" style="width: 900px; height: 500px;"></div>
+		</table>
 	</body>
 </html>
