dd5ea40d0011e7b776cdbbdcf6c12ca37344dad9
fandayp <13514023@std.stei.itb.ac.id>
2018-05-02 17:29:49 +0700

    filter check box

--- drawChart.php
+++ drawChart.php
@@ -1,12 +1,16 @@
+<?php include 'filter.php'; ?>
 <script type="text/javascript">
 	function getData() {
+		var checkedMedal = getMedalFilterValue();
+		console.log("checkedMedal", checkedMedal);
+
 		// READ CSV FILE
 		var year = $("#year").val();
-		
+
 		<?php include 'readcsv.php'; ?>
-		
-		var medal = $("input:radio[name=medal]:checked").val();
-		// console.log(medal);
+
+		// var medal = $("[name=medal]");
+		// console.log("medal", medal);
 
 		// PHP array to JS array
 		// console.log("loop" + element);
@@ -14,8 +18,8 @@
 			$php_country_array = $country_array;
 			$js_country_array = json_encode($php_country_array);
 			echo "var country_array = ". $js_country_array . ";\n";
-		?>	
-		
+		?>
+		var count = 0;
 		var country = ["Thailand", "Indonesia", "Malaysia", "Philippines", "Singapore", "Vietnam", "Myanmar", "Laos", "Cambodia", "Brunei", "Timor-Leste"];
 		country.forEach(function(current_country) {
 			// filtering
@@ -24,51 +28,68 @@
 				for (var j = 1; j < cabor_array.length; j++) { // string to int
 					cabor_array[j] = parseInt(cabor_array[j]);
 				}
-	
-				if (medal == "gold") {
-					cabor_array[2] = 0;
-					cabor_array[3] = 0;
-					// cabor_array.splice(2, 2);
-				} else if (medal == "silver") {
-					// cabor_array.splice(1, 1);
-					// cabor_array.splice(2, 1);
-					cabor_array[1] = 0;
-					cabor_array[3] = 0;
-				} else if (medal == "brown") {
-					// cabor_array.splice(1, 2);
-					cabor_array[1] = 0;
-					cabor_array[2] = 0;
+
+
+				if (!checkedMedal.includes("0")) {
+					if (!checkedMedal.includes("1")) {
+							cabor_array[1] = 0;
+					}
+					if (!checkedMedal.includes("2")) {
+							cabor_array[2] = 0;
+					}
+					if (!checkedMedal.includes("3")) {
+							cabor_array[3] = 0;
+					}
 				}
-				
-				cabor_array.push(medal);
+
+				// 	cabor_array[2] = 0;
+				// 	cabor_array[3] = 0;
+				// 	// cabor_array.splice(2, 2);
+				// } else if (medal == "silver") {
+				// 	// cabor_array.splice(1, 1);
+				// 	// cabor_array.splice(2, 1);
+				// 	cabor_array[1] = 0;
+				// 	cabor_array[3] = 0;
+				// } else if (medal == "brown") {
+				// 	// cabor_array.splice(1, 2);
+				// 	cabor_array[1] = 0;
+				// 	cabor_array[2] = 0;
+				// }
+
+				// cabor_array.push(medal);
 			}
 			// console.log(country_array);
-	
+
 			// chart
 			google.charts.load("current", {packages:["corechart"]});
 			google.charts.setOnLoadCallback(drawChart);
-	
-			var header = [["Country", "Gold", "Silver", "Bronze", { role: "style" }]];
+
+			var header = [["Country", "Gold", "Silver", "Bronze"]];
 			var d = header.concat(country_array[current_country]);
 
+			console.log(d);
+
+			count++;
+
+			console.log(count);
 
 			function drawChart() {
 				// console.log("drawChart" + element);
 			  var data = google.visualization.arrayToDataTable(d);
 
 			  var view = new google.visualization.DataView(data);
-			  
-			  if (medal == null) {
-			  	var isStacked = "percent";
-			  } else {
-			  	var isStacked = "absolute";
-			  }
+
+			  // if (medal == null) {
+			  // 	var isStacked = "percent";
+			  // } else {
+			  // 	var isStacked = "absolute";
+			  // }
 
 			  var options = {
 			    legend: { position: "none" },
 			    hAxis: { textPosition: "none"},
 			    // vAxis: { textPosition: "none"},
-			    isStacked: isStacked,
+			    isStacked: true,
 			    series: {
 		          0:{color:"gold"},
 		          1:{color:"silver"},
